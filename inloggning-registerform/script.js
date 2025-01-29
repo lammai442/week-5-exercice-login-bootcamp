19e15d866ea6897f068bf71e33729a9e673ba469
lammai442 <lam.mai442@gmail.com>
2025-01-29 23:24:31 +0100

    Worked on validateForm

--- inloggning-registerform/script.js
+++ inloggning-registerform/script.js
@@ -1,3 +1,4 @@
+const users = [];
 let registerFormRef = document.querySelector('#registerForm');
 registerFormRef.classList.add('d-none');
 
@@ -6,16 +7,16 @@ let loginFormRef = document.querySelector('#loginForm');
 let registerAccountBtnRef = document.querySelector('#registerAccountBtn');
 
 // Login sidan - Om användaren klickar på registrera konto så körs följande.
-loginFormRef.addEventListener('submit', (event) => {
+registerAccountBtnRef.addEventListener('click', (event) => {
     // Här göms loginformuläret och den andra syns. Event skickas med då .preventDefault() kräver det för att köras i nedanstående funktion.
     swapRegisterLoginForm(event);
 });
 
 // Register sidan - Om användaren klickar på 'Till logga in' så körs följande.
-registerFormRef.addEventListener('submit', (event) => {
-    // Här göms registerformuläret och den andra syns. Event skickas med då .preventDefault() kräver det för att köras i nedanstående funktion.
-    swapRegisterLoginForm(event);
-});
+// registerFormRef.addEventListener('submit', (event) => {
+//     // Här göms registerformuläret och den andra syns. Event skickas med då .preventDefault() kräver det för att köras i nedanstående funktion.
+//     swapRegisterLoginForm(event);
+// });
 
 // Funktion för att toggla mellan klassen 'd-none' där den antingen lägger till eller tar bort klassen.
 function swapRegisterLoginForm(event) {
@@ -32,17 +33,69 @@ const passwordConfirmRef = document.querySelector('#passwordConfirm');
 const checkboxRef = document.querySelector('#checkbox');
 const registerBtnRef = document.querySelector('#registerBtn');
 
-registerBtnRef.addEventListener('click', () => {
+// Knapp för att registrera en ny användare. Det kommer göras validering
+registerBtnRef.addEventListener('click', (event) => {
+    // Förhindrar att formuläret uppdaterar sidan.
+    event.preventDefault();
     if (validateRegistration()) {
-        // lagra innehållet ti
+        // lagra innehållet till databasen med user
+        console.log('Formuläret är skickat');
+    } else {
+        console.log('Du har fått fel');
     }
 });
 
 function validateRegistration() {
-    // username måste bestå av minst 6 tecken
+    const userNameRef = document.querySelector('#userName');
+    const passwordRef = document.querySelector('#password');
+    const passwordConfirmRef = document.querySelector('#passwordConfirm');
+    const checkboxRef = document.querySelector('#checkbox');
+    const registerBtnRef = document.querySelector('#registerBtn');
+    const errorMsgRef = document.querySelector('#errorMsg-register');
+    const labelRef = document.querySelector('#label');
+
+    try {
+        // Kontrollerar att användarnamnet inte är kortare än 6 texten.
+        if (userNameRef.value.length < 6) {
+            throw {
+                message: 'Du måste ange mer än 6 tecken',
+                nodeRef: userNameRef,
+            };
+        } // password måste bestå av minst 8 tecken
+        else if (passwordRef.value.length < 8) {
+            throw {
+                message: 'Ditt lösenord är för kort. Du måste fylla i minst 8 tecken.',
+                nodeRef: passwordRef,
+            };
+        } // password och password again måste vara identiska
+        else if (!(passwordRef.value === passwordConfirmRef.value)) {
+            throw {
+                message: 'Dina lösenord stämmer inte överens. Fyll i rätt lösenord.',
+                nodeRef: passwordConfirmRef,
+            };
+        } // checkboxen måste vara checked
+        else if (!checkboxRef.checked) {
+            throw {
+                message: 'Du måste godkänna för registring',
+                nodeRef: checkboxRef,
+            };
+        }
+        userNameRef.value = '';
+        passwordRef.value = '';
+        passwordConfirmRef.value = '';
+        checkboxRef.checked = false;
+
+        return true;
+    } catch (error) {
+        errorMsgRef.textContent = error.message;
+        error.nodeRef.value = '';
+        console.log(error);
+
+        error.nodeRef.focus();
+    }
+    return false;
+
     // username får inte vara upptaget
-    // password måste bestå av minst 8 tecken
-    // password och password again måste vara identiska
-    // checkboxen måste vara checked
+
     console.log('validateRegistration');
 }
