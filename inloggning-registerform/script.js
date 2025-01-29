9e9e7d9a7354cc7b458c9dd54c2b0a46ea870a3a
lammai442 <lam.mai442@gmail.com>
2025-01-29 15:22:52 +0100

    Continue with register login

--- inloggning-registerform/script.js
+++ inloggning-registerform/script.js
@@ -1,8 +1,48 @@
-let startFormRef = document.querySelector('#startForm');
+let registerFormRef = document.querySelector('#registerForm');
+registerFormRef.classList.add('d-none');
 
-startFormRef.addEventListener('submit', (event) => {
-    // Denna förhindrar att sidan laddar om sig efter att ha lyssnat på 'submit'
-    event.preventDefault();
-    // Gömmer startFormen genom att lägga en d-none klass.
-    startFormRef.classList.add('d-none');
+let loginFormRef = document.querySelector('#loginForm');
+
+let registerAccountBtnRef = document.querySelector('#registerAccountBtn');
+
+// Login sidan - Om användaren klickar på registrera konto så körs följande.
+loginFormRef.addEventListener('submit', (event) => {
+    // Här göms loginformuläret och den andra syns. Event skickas med då .preventDefault() kräver det för att köras i nedanstående funktion.
+    swapRegisterLoginForm(event);
 });
+
+// Register sidan - Om användaren klickar på 'Till logga in' så körs följande.
+registerFormRef.addEventListener('submit', (event) => {
+    // Här göms registerformuläret och den andra syns. Event skickas med då .preventDefault() kräver det för att köras i nedanstående funktion.
+    swapRegisterLoginForm(event);
+});
+
+// Funktion för att toggla mellan klassen 'd-none' där den antingen lägger till eller tar bort klassen.
+function swapRegisterLoginForm(event) {
+    // Denna gör så att sidan inte laddas om efter submit. Parametern event behövs här för att funktionen ska köras.
+    event.preventDefault();
+    registerFormRef.classList.toggle('d-none');
+    loginFormRef.classList.toggle('d-none');
+}
+
+////REGISTER////
+const userNameRef = document.querySelector('#userName');
+const passwordRef = document.querySelector('#password');
+const passwordConfirmRef = document.querySelector('#passwordConfirm');
+const checkboxRef = document.querySelector('#checkbox');
+const registerBtnRef = document.querySelector('#registerBtn');
+
+registerBtnRef.addEventListener('click', () => {
+    if (validateRegistration()) {
+        // lagra innehållet ti
+    }
+});
+
+function validateRegistration() {
+    // username måste bestå av minst 6 tecken
+    // username får inte vara upptaget
+    // password måste bestå av minst 8 tecken
+    // password och password again måste vara identiska
+    // checkboxen måste vara checked
+    console.log('validateRegistration');
+}
