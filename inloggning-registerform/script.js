26569163b1f661235ec57c95fae2b5c2e350f7f6
lammai442 <lam.mai442@gmail.com>
2025-01-30 16:54:26 +0100

    Done with register form

--- inloggning-registerform/script.js
+++ inloggning-registerform/script.js
@@ -1,22 +1,21 @@
-const users = [];
+// Array för att stoppa in alla värden som kommer skickas till LocalStorage
+let users = getDataFromLocalStorage('users');
+
+// Referens till loginformuläret
+let loginFormRef = document.querySelector('#loginForm');
+
+// Referens till registeringsformuläret
 let registerFormRef = document.querySelector('#registerForm');
 registerFormRef.classList.add('d-none');
 
-let loginFormRef = document.querySelector('#loginForm');
-
-let registerAccountBtnRef = document.querySelector('#registerAccountBtn');
+// Referens till knappen för att registreraformuläret.
+let toRegisterFormBtnRef = document.querySelector('#toRegisterFormBtn');
 
 // Login sidan - Om användaren klickar på registrera konto så körs följande.
-registerAccountBtnRef.addEventListener('click', (event) => {
-    // Här göms loginformuläret och den andra syns. Event skickas med då .preventDefault() kräver det för att köras i nedanstående funktion.
-    swapRegisterLoginForm(event);
+toRegisterFormBtnRef.addEventListener('click', (event) => {
+    // Här göms loginformuläret och registreringsformuläret syns. 
+    swapRegisterLoginForm(event);    
 });
-
-// Register sidan - Om användaren klickar på 'Till logga in' så körs följande.
-// registerFormRef.addEventListener('submit', (event) => {
-//     // Här göms registerformuläret och den andra syns. Event skickas med då .preventDefault() kräver det för att köras i nedanstående funktion.
-//     swapRegisterLoginForm(event);
-// });
 
 // Funktion för att toggla mellan klassen 'd-none' där den antingen lägger till eller tar bort klassen.
 function swapRegisterLoginForm(event) {
@@ -26,42 +25,100 @@ function swapRegisterLoginForm(event) {
     loginFormRef.classList.toggle('d-none');
 }
 
-////REGISTER////
+// LOGIN-FORM //
+const userNameLoginRef = document.querySelector('#userNameLogin');
+const passwordLoginRef = document.querySelector('#passwordLogin');
+const loginBtnRef = document.querySelector('#loginBtn');
+
+loginBtnRef.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    
+    if(validateLogin()) {
+        loginFormRef.classList.toggle('d-none');
+        let welcomeMsg = document.createElement('h2');
+        welcomeMsg.textContent = `Välkommen ${userNameLoginRef.value}`;
+        let bodyRef = document.querySelector('body')
+        bodyRef.appendChild(welcomeMsg);
+    }
+});
+
+function validateLogin () { 
+    let titleLoginRef = document.querySelector('#titleLogin').insertAdjacentElement('afterend', infoMsg)
+
+    infoMsg.classList.add('error-msg');
+    try {
+        if(!doesUserExist(userNameLoginRef.value)) {
+            throw {
+                message: 'Det användarnamnet existerar inte',
+                nodeRef: userNameLoginRef
+            }
+        }
+        else if ()
+        return true;
+    } catch (error) {
+        infoMsg.textContent = error.message
+        nodeRef.focus();
+    } return false;
+};
+
+// REGISTER-FORM //
 const userNameRef = document.querySelector('#userName');
 const passwordRef = document.querySelector('#password');
 const passwordConfirmRef = document.querySelector('#passwordConfirm');
 const checkboxRef = document.querySelector('#checkbox');
 const registerBtnRef = document.querySelector('#registerBtn');
 
-// Knapp för att registrera en ny användare. Det kommer göras validering
+// Här skapas inforutan som p element.
+const infoMsg = document.createElement('p');
+infoMsg.id = 'infoMsgRef';
+
+
+// Knapp för att registrera en ny användare. Det kommer göras en validering.
 registerBtnRef.addEventListener('click', (event) => {
     // Förhindrar att formuläret uppdaterar sidan.
     event.preventDefault();
     if (validateRegistration()) {
-        // lagra innehållet till databasen med user
-        console.log('Formuläret är skickat');
-    } else {
-        console.log('Du har fått fel');
+        // Här körs funktion för att spara nya användaren i LocalStorage
+        saveUserToLocalStorage()
+        // Här töms alla värden när all validering gått igenom.
+        infoMsgRef.classList.toggle('error-msg');      
+        infoMsgRef.textContent = `Ditt konto ${userNameRef.value} är skapat`;
+        userNameRef.value = '';
+        passwordRef.value = '';
+        passwordConfirmRef.value = '';
+        checkboxRef.checked = false;
+        // Här tas inforutan bort efter 4 s.
+        removeElementTimer(infoMsgRef, 4000);        
     }
 });
 
+// Funktion som körs för att validera en ny registrering.
 function validateRegistration() {
-    const userNameRef = document.querySelector('#userName');
-    const passwordRef = document.querySelector('#password');
-    const passwordConfirmRef = document.querySelector('#passwordConfirm');
-    const checkboxRef = document.querySelector('#checkbox');
-    const registerBtnRef = document.querySelector('#registerBtn');
-    const errorMsgRef = document.querySelector('#errorMsg-register');
-    const labelRef = document.querySelector('#label');
+    // Lägger till infoMsg efter h2 i registerform. Först måste man hämta hem den första elementet och sedan tilldela .insertAdjacentElement
+    let registerTitleRef = document.querySelector('#registerTitle').insertAdjacentElement('afterend', infoMsg);   
+    // Lägger till den röda texten till infoMsg genom klassen 'error-msg'
+    infoMsg.classList.add('error-msg');
+    
+    // Hämtar hem den senaste databasen.
+    users = getDataFromLocalStorage('users');
 
     try {
         // Kontrollerar att användarnamnet inte är kortare än 6 texten.
-        if (userNameRef.value.length < 6) {
+        if (userNameRef.value.length < 6 || userNameRef.value.includes(' ')) {
             throw {
-                message: 'Du måste ange mer än 6 tecken',
+                message: 'Användarnamnet måste vara mer än 6 tecken och får inte innehålla mellanslag.',
                 nodeRef: userNameRef,
             };
-        } // password måste bestå av minst 8 tecken
+        }
+        // Här används .some för att kolla över arrayen users som sparar all data från LocalStorage. 'user' är varje objekt i arrayen och kollar det objektets keyname 'userName' och jämför det med inputvalue från userNameRef.
+        else if(users.some(user => user.userName === userNameRef.value)) {
+            throw {
+                message: 'Användarnamnet är upptagen, välj ett annat.',
+                nodeRef: userNameRef
+            }
+        }
+        // password måste bestå av minst 8 tecken
         else if (passwordRef.value.length < 8) {
             throw {
                 message: 'Ditt lösenord är för kort. Du måste fylla i minst 8 tecken.',
@@ -70,7 +127,7 @@ function validateRegistration() {
         } // password och password again måste vara identiska
         else if (!(passwordRef.value === passwordConfirmRef.value)) {
             throw {
-                message: 'Dina lösenord stämmer inte överens. Fyll i rätt lösenord.',
+                message: 'Ditt lösenord stämmer inte överens. Fyll i rätt lösenord.',
                 nodeRef: passwordConfirmRef,
             };
         } // checkboxen måste vara checked
@@ -80,22 +137,60 @@ function validateRegistration() {
                 nodeRef: checkboxRef,
             };
         }
-        userNameRef.value = '';
-        passwordRef.value = '';
-        passwordConfirmRef.value = '';
-        checkboxRef.checked = false;
-
         return true;
-    } catch (error) {
-        errorMsgRef.textContent = error.message;
+    } 
+    catch (error) {
+        // Ger felmeddelande beroende på vart den fastnar i trysatsen.        
+        infoMsgRef.textContent = error.message;
+        // Tömmer den specifika innehållet som den fastnar på.
         error.nodeRef.value = '';
-        console.log(error);
-
+        // Ger en fokusram runt där den ger error.
         error.nodeRef.focus();
     }
     return false;
+}
 
-    // username får inte vara upptaget
+console.log(users);
 
-    console.log('validateRegistration');
+function saveUserToLocalStorage() {
+    let userObject = {};
+    // Här sparas samtliga datainput som gjorts i ett nytt objekt.
+    userObject.userName = userNameRef.value;
+    userObject.password = passwordRef.value;
+    userObject.passwordConfirmed = passwordConfirmRef.value;
+    // console.log(userObject);
+    // // Här läggs objekten in i arrayen users.
+    // users.push(userObject);
+    // Här hämtas den befintliga arrayen i localStorage och argumentetet 'users' som ska sökas efter.
+    let usersArray = getDataFromLocalStorage('users')
+    // Här läggs den nya objektet in i den befintliga arrayen i LocalStorage.
+    usersArray.push(userObject);
+    // Här skrivs det över den befintliga 'users' i LocalStorage.
+    localStorage.setItem('users', JSON.stringify(usersArray));
+}
+
+// Funktionen tar in en input för att söka efter det keynamnet i LocalStorage. Har anget 'users' som skickas med som argument.
+function getDataFromLocalStorage(input) {
+    // Om 'users' inte finns med så kommer det istället tilldelas en tom array.
+    const getData = localStorage.getItem(input) || '[]';
+    // Då en hämtning från localstorage är i sträng så måste den omvandlas genom JSON.parse för att bli en array.    
+    return JSON.parse(getData);
+}
+
+function removeElementTimer(element, time) {
+
+    setTimeout(() => {
+        element.remove();
+    }, time)
+};
+
+function doesUserExist(input) {
+    let storedData = JSON.parse(localStorage.getItem('users'));
+   
+    let name = storedData.map(user => user.userName);
+    if (name.includes(input)) {
+        return true
+    } else {
+        return false
+    }
 }
